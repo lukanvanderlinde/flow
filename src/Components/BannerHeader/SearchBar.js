a9b1d673a1d5ca8724107a487e791c827bc1574d
Lukan Vanderlinde <lukanvanderlinde@gmail.com>
2020-02-05 14:34:29 -0300

    last edit

--- src/Components/BannerHeader/SearchBar.js
+++ src/Components/BannerHeader/SearchBar.js
@@ -5,8 +5,10 @@ import { Grid, TextField, Button } from '@material-ui/core'
 import { DatePicker } from '@material-ui/pickers'
 
 function Search() {
-  const [startDate, setStartDate] = React.useState(new Date())
-  const [returnDate, setReturnDate] = React.useState(new Date())
+  const [startDate, setStartDate] = React.useState(null)
+  const [returnDate, setReturnDate] = React.useState(null)
+  const [originPlace, setOriginPlace] = React.useState(null)
+  const [destinationPlace, setDestinationPlace] = React.useState(null)
 
   const handleStartDate = (date) => {
     setStartDate(date)
@@ -14,6 +16,18 @@ function Search() {
   const handleReturnDate = (date) => {
     setReturnDate(date)
   }
+  const handleOriginPlace = (place) => {
+    setOriginPlace(place)
+  }
+  const handleDestinationPlace = (place) => {
+    setDestinationPlace(place)
+  }
+  const handleSearch = (event) => {
+    console.log(
+      `Origem: ${originPlace} \n Destino: ${destinationPlace} \n Ida: ${startDate} \n Volta: ${returnDate}`
+    )
+  }
+
   return (
     <Grid container spacing={4}>
       {/* INPUTS */}
@@ -32,15 +46,20 @@ function Search() {
                   placeholder='De onde vamos partir?'
                   type='search'
                   variant='outlined'
+                  onChange={(event) => {
+                    handleOriginPlace(event.target.value)
+                  }}
                 />
               </Grid>
               <Grid item>
                 <DatePicker
+                  format='DD/MM/YYYY'
                   label='Ida'
                   inputVariant='outlined'
                   value={startDate}
                   onChange={handleStartDate}
                   animateYearScrolling
+                  minDate={new Date()}
                 />
               </Grid>
             </Grid>
@@ -58,15 +77,23 @@ function Search() {
                   placeholder='Para onde você vai?'
                   type='search'
                   variant='outlined'
+                  onChange={(event) => {
+                    handleDestinationPlace(event.target.value)
+                  }}
                 />
               </Grid>
               <Grid item>
                 <DatePicker
+                  format='DD/MM/YYYY'
                   label='Volta (opcional)'
                   inputVariant='outlined'
                   value={returnDate}
                   onChange={handleReturnDate}
                   animateYearScrolling
+                  minDate={startDate}
+                  minDateMessage={
+                    'Data de volta não pode ser antes do que a de ida :)'
+                  }
                 />
               </Grid>
             </Grid>
@@ -77,7 +104,12 @@ function Search() {
       {/* Buttom */}
       <Grid item xs={12}>
         <Grid container justify='center' alignItems='center' spacing={2}>
-          <Button variant='contained' color='primary'>
+          <Button
+            variant='contained'
+            color='primary'
+            onClick={(event) => {
+              handleSearch(event)
+            }}>
             Buscar minha viagem
           </Button>
         </Grid>
