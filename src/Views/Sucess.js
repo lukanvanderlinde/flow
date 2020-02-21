f0a6e64b3aef6f36827ccfe88b103b1db7c4e1f9
Lukan Vanderlinde <lukanvanderlinde@gmail.com>
2020-02-21 12:07:43 -0300

    Implementada tela de sucesso

--- src/Views/Sucess.js
+++ src/Views/Sucess.js
@@ -5,7 +5,7 @@ import Lottie from 'react-lottie'
 import LottieSucess from 'Assets/Animation/LottieSucess.json'
 
 // * Componentes
-import { Container } from '@material-ui/core'
+import { Container, Paper, Box, Typography } from '@material-ui/core'
 
 function Sucess() {
   const defaultOptions = {
@@ -16,7 +16,29 @@ function Sucess() {
 
   return (
     <Container maxWidth='xl'>
+      <Box textAlign='center' marginTop='2rem'>
+        <Typography variant='h2'>
+          Uhull! Tudo certo com a sua viagem!
+        </Typography>
+        <Typography variant='body1'>
+          Agora enviaremos a sua confirmação no seu whatsapp e email.
+        </Typography>
+      </Box>
       <Lottie options={defaultOptions} height={200} width={200} />
+      <Box marginTop='1rem' paddingBottom='10rem'>
+        <Paper>
+          <Box padding='1rem' paddingBottom='1rem' marginBottom='1rem'>
+            <Typography variant='body1'>
+              - Fique tranquilo, nós fazemos uma pré reserva no seu cartão e a
+              cobrança vem depois.
+            </Typography>
+            <Typography variant='body1'>
+              - Você só precisa da confirmação do seu whatsapp e um documento
+              com foto.
+            </Typography>
+          </Box>
+        </Paper>
+      </Box>
     </Container>
   )
 }
